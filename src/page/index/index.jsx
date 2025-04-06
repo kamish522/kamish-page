a5bc892bdb77dc9853cfeea7b4eb866a9670bd8a
Kash <ipomainkra@gmail.com>
2025-04-06 18:10:34 +1000

    adit files

--- src/page/index/index.jsx
+++ src/page/index/index.jsx
@@ -1,11 +1,13 @@
 import About from "./about.jsx";
-import Game from "./game-stats.jsx";
+import Game from "./info.jsx";
+import Invite from "./invite.jsx";
 
 const Index = () => {
     return (
         <>
             <About/>
             <Game/>
+            <Invite/>
         </>
     )
 }
